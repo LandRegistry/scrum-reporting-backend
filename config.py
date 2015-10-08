744eddb57667e78fdd8a64368c8c0014bd791707
Andrew Moore <mooreandrewlr@gmail.com>
2015-10-08 09:07:51 +0100

    Updated App

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ import os
 class Config(object):
     DEBUG = False
     LOGGING_PATH = os.getenv('LOGGING_PATH', 'python_logging/logging.yaml')
-    SECRET_KEY = os.environ['SECRET_KEY']
 
 class DevelopmentConfig(Config):
     DEBUG = True
